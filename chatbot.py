631b53d30eeab93b9e08c211bffb7057e17e6f41
Tommy <86322453+TechyTommy3@users.noreply.github.com>
2021-07-30 11:55:55 -0500

    Fix memory

--- chatbot.py
+++ chatbot.py
@@ -1,26 +1,14 @@
 import pickle
 import os
 import json
-if os.path.exists("tommybot.mem") == True or False:
-    with open('tommybot.mem', 'w') as tempmemory:
-        chatbotname = input("What do you want your chatbot to be named?")
-        data = {"name": chatbotname, "status": "new"}
-        tempmemory = open("tommybot.mem", "w")
-        jsony = json.dumps(data, separators=(',', ':')) + "\n"
-        print(jsony)
-        tempmemory.write(jsony)
-        tempmemory.flush()
-        tempmemory.close()
+chatbotdata = input("Enter in your chatbot data: ")
 print("TommyChatbot")
-print("Reading memory...")
-memoryread = open("tommybot.mem", "r")
-memorywrite = open("tommybot.mem", "w")
-memory = json.loads(memoryread.read())
-memoryread.close()
+print("Decoding data...")
+memory = json.loads(chatbotdata)
 if memory["status"] == "new":
     print("Hi! My name is " + memory[name] + ". What's your name?")
     memory["yourname"] = input("You: ")
-    pickle.dump(memory, memorywrite)
+    chatbotdata["yourname"] = memory["yourname"]
     print("Hi " + memory["yourname"] + "!")
     print("Anyways, isn't the Chatbot Network great?")
     answer = input("You: ")
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-import pickle
+import json
+print("Notice: TommyChatbot 1.0 does not have proper saving.")
 print("TommyChatbot Setup")
 print("This will WIPE ALL YOUR TommyChatbot memory. Press enter to confirm, or exit now.")
 chatbotname = input("What do you want your chatbot to be named?")
 data = {"name": chatbotname, "status": "new"}
-memory = open("tommybot.mem", "wb")
-pickle.dump(data, memory)
-print("Suffeculy set up new TommyChatbot data on file tommybot.mem.")
-memory.close()
+jsondat = json.dumps(data, separators=(',', ':'))
+#print("Suffeculy set up new TommyChatbot data on file tommybot.mem.")
+print("Needed data: " + jsondat)
 input()
